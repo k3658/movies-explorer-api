257877460b00fc1ee2a8d81743269c67c568275d
kk36 <lk0981@mail.ru>
2023-08-23 13:46:05 +0300

    remove node_modules

--- controllers/movies.js
+++ controllers/movies.js
@@ -57,7 +57,7 @@ const createMovie = (req, res, next) => {
 
 // DELETE
 const deleteMovie = (req, res, next) => {
-  const movieId = req.params;
+  const { movieId } = req.params;
   const owner = req.user._id;
 
   Movie.findById(movieId)
